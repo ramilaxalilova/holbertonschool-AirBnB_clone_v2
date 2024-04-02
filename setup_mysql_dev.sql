4cc6e5bbcc76aecee17ffdbb032672e48f3ac9a5
ramilaxalilova <xalilovaramila@yahoo.com>
2024-04-02 12:31:55 +0400

    some changes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,13 @@
--- script that prepares a MySQL server for the projec
+-- creates new database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
+-- creates new user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- grant all privileges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@localhost;
+
+-- grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@localhost;
+
+FLUSH PRIVILEGES;
