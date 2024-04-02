bf534bce4973cb956c770774ccd1c9c9fa8a59fb
ramilaxalilova <xalilovaramila@yahoo.com>
2024-04-02 11:47:31 +0400

    amenity

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
-import sqlalchemy
 
 
 Base = declarative_base()
