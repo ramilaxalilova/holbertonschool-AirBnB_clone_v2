ac811c1f8ee5da3d606e808499ef57526cb05c20
root <garazadeisa625@gmail.com>
2024-04-03 19:17:07 +0000

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,12 +1,12 @@
-uu#!/usr/bin/python3
+#!/usr/bin/python3
 """ Module for testing file storage"""
 import unittest
 from models.base_model import BaseModel
-from models import storage, type_of_storage
-import os
+from models import storage
+from os import getenv, remove, path
 
 
-@unittest.skipIf(type_of_storage == "db", "Storage type: Database")
+@unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "DBStorage")
 class test_fileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
@@ -21,7 +21,7 @@ class test_fileStorage(unittest.TestCase):
     def tearDown(self):
         """ Remove storage file at end of tests """
         try:
-            os.remove('file.json')
+            remove('file.json')
         except:
             pass
 
@@ -46,7 +46,7 @@ class test_fileStorage(unittest.TestCase):
     def test_base_model_instantiation(self):
         """ File is not created on BaseModel save """
         new = BaseModel()
-        self.assertFalse(os.path.exists('file.json'))
+        self.assertFalse(path.exists('file.json'))
 
     def test_empty(self):
         """ Data is saved to file """
@@ -54,13 +54,13 @@ class test_fileStorage(unittest.TestCase):
         thing = new.to_dict()
         new.save()
         new2 = BaseModel(**thing)
-        self.assertNotEqual(os.path.getsize('file.json'), 0)
+        self.assertNotEqual(path.getsize('file.json'), 0)
 
     def test_save(self):
         """ FileStorage save method """
         new = BaseModel()
         storage.save()
-        self.assertTrue(os.path.exists('file.json'))
+        self.assertTrue(path.exists('file.json'))
 
     def test_reload(self):
         """ Storage file is successfully loaded to __objects """
@@ -85,7 +85,7 @@ class test_fileStorage(unittest.TestCase):
         """ BaseModel save method calls storage save """
         new = BaseModel()
         new.save()
-        self.assertTrue(os.path.exists('file.json'))
+        self.assertTrue(path.exists('file.json'))
 
     def test_type_path(self):
         """ Confirm __file_path is string """
