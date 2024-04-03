28f9e74c50ca2f4e47a9d7db74c411a0370727d8
root <garazadeisa625@gmail.com>
2024-04-03 19:01:08 +0000

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,6 +6,13 @@ import datetime
 from uuid import UUID
 import json
 import os
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.amenity import Amenity
+from models import type_of_storage
 
 
 class test_basemodel(unittest.TestCase):
@@ -24,7 +31,7 @@ class test_basemodel(unittest.TestCase):
     def tearDown(self):
         try:
             os.remove('file.json')
-        except:
+        except FileNotFoundError:
             pass
 
     def test_default(self):
@@ -47,6 +54,7 @@ class test_basemodel(unittest.TestCase):
         with self.assertRaises(TypeError):
             new = BaseModel(**copy)
 
+    @unittest.skipIf(type_of_storage == "db", "Storage type: Database")
     def test_save(self):
         """ Testing save """
         i = self.value()
@@ -59,7 +67,7 @@ class test_basemodel(unittest.TestCase):
     def test_str(self):
         """ """
         i = self.value()
-        self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
+        self.assertEqual(str(i), '[{}] ({}) {}'.format(i.__class__.__name__, i.id,
                          i.__dict__))
 
     def test_todict(self):
@@ -74,6 +82,12 @@ class test_basemodel(unittest.TestCase):
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
+    def test_kwargs_one(self):
+        """ """
+        n = {'Name': 'test'}
+        new = self.value(**n)
+        self.assertEqual(type(new.id), str)
+
     def test_id(self):
         """ """
         new = self.value()
@@ -83,3 +97,11 @@ class test_basemodel(unittest.TestCase):
         """ """
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
+
+    def test_updated_at(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.updated_at), datetime.datetime)
+        n = new.to_dict()
+        new = self.value(**n)
+        self.assertTrue(n['created_at'] != new.updated_at)
