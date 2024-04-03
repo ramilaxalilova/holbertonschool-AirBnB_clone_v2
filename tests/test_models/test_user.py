1521a6398e2fd945ec6ae6451522866c6a515b9c
root <garazadeisa625@gmail.com>
2024-04-03 19:15:35 +0000

    test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -12,10 +12,11 @@ class test_User(test_basemodel):
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
-        self.user1 = User(first_name='Isa',
-                          last_name='Guliyev',
-                          email='isa.guliyev@holbertonstudents.com',
-                          password='808&Heartbreak')
+        self.user1 = User(
+            first_name='Joe',
+            last_name='DeGuzman',
+            email='heeqaqahelemiolub@gmail.com',
+            password='salam123')
 
     def test_first_name(self):
         """ """
