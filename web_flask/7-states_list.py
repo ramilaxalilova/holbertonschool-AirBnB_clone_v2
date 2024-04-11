58977e5fd7f0d74c0913895c4cd427eaeb7835fa
ramilaxalilova <xalilovaramila@yahoo.com>
2024-04-11 20:17:52 +0400

    modeify

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-""" Start flask service that does something. """
-
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def appcontext_teardown(self):
-    """use storage for fetching data from the storage engine
-    """
+def tear_down(self):
+    """tear down app context"""
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
-def state_info():
-    """Display a HTML page inside the tag BODY"""
-    return render_template('7-states_list.html',
-                           states=storage.all(State))
+def list_states():
+    """lists states from database"""
+    dict_states = storage.all(State)
+    all_states = []
+    for k, v in dict_states.items():
+        all_states.append(v)
+    return render_template('7-states_list.html', all_states=all_states)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
