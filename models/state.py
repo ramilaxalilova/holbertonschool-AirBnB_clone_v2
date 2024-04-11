499e603ebc220db8fd809463c9e8609ba1b8f674
ramilaxalilova <xalilovaramila@yahoo.com>
2024-04-11 22:32:50 +0400

    modify state

--- models/state.py
+++ models/state.py
@@ -10,9 +10,13 @@ import os
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
                           cascade="all, delete-orphan")
+
+    else:
+        name = ""
 
     @property
     def cities(self):
