58ea06dc37d73f9910aa4131a57dee19cf108eea
ramilaxalilova <xalilovaramila@yahoo.com>
2024-04-11 20:22:04 +0400

    changes state list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""Sript that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 
 app = Flask(__name__)
@@ -16,11 +16,8 @@ def tear_down(self):
 @app.route('/states_list', strict_slashes=False)
 def list_states():
     """lists states from database"""
-    dict_states = storage.all(State)
-    all_states = []
-    for k, v in dict_states.items():
-        all_states.append(v)
-    return render_template('7-states_list.html', all_states=all_states)
+    states = storage.all('State').values()
+    return render_template("7-states_list.html", states=states)
 
 
 if __name__ == "__main__":
