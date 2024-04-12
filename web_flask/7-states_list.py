32ae555854f3b32f3bb4ff2090d0e2ccfbd7123e
ramilaxalilova <xalilovaramila@yahoo.com>
2024-04-12 08:58:59 +0400

    changed 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,20 +3,26 @@
 from flask import Flask, render_template
 from models import *
 from models import storage
+from models.state import State
 app = Flask(__name__)
-
-
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    """lists states from database"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template("7-states_list.html", states=states)
-
 
 @app.teardown_appcontext
 def tear_down(self):
     """tear down app context"""
     storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """lists states from database"""
+    dict_states = storage.all(State)
+    all_states = []
+    for k, v in dict_states.items():
+        all_states.append(v)
+    return render_template('7-states_list.html', all_states=all_states)
+
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
