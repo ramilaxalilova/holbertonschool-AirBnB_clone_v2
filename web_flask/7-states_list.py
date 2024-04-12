95e0031f0f9c656ba1cd9af3ef08ca5847270c7b
ramilaxalilova <xalilovaramila@yahoo.com>
2024-04-12 08:36:12 +0400

    drgfg

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Script that starts a Flask web application"""
 from flask import Flask, render_template
+from models import *
 from models import storage
 app = Flask(__name__)
 
